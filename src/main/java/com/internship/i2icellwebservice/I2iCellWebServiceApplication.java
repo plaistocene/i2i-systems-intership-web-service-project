1e76e83c89972bc24e867d0604b6da1bab8b2b82
I. Bugra Aslan <bugra.sln@gmail.com>
2021-09-23 09:50:33 +0300

    initial commit

---
+++ src/main/java/com/internship/i2icellwebservice/I2iCellWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.internship.i2icellwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I2iCellWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(I2iCellWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/internship/i2icellwebservice/I2iCellWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internship.i2icellwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I2iCellWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
